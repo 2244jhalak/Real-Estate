bf5dd8a4a57a3ee1d09a36e97793623e7d548da9
Jhalak <stdjhalak@gmail.com>
2024-04-09 22:41:30 +0600

    header and banner section added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,22 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import Cover1 from '../../../src/assets/images/Cover1.jpg'
+import Cover2 from '../../../src/assets/images/Cover2.jpg'
+import Cover3 from '../../../src/assets/images/Cover3.jpeg'
+
+const Banner = () => {
+    return (
+        <div className='my-10'>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                 <SwiperSlide><img className='w-full h-[580px] rounded-lg' src={Cover1} alt="" /></SwiperSlide>
+                 <SwiperSlide><img className='w-full h-[580px] rounded-lg' src={Cover2} alt="" /></SwiperSlide>
+                 <SwiperSlide><img className='w-full h-[580px] rounded-lg' src={Cover3} alt="" /></SwiperSlide>
+            </Swiper>
+            
+        </div>
+    );
+};
+
+export default Banner;
